f0b5a2210fb1478d230dd890a13f3d62f47d55d1
Jojoedev <odogwujonas@gmail.com>
2023-08-19 20:59:06 +0100

    added changes

--- WebAPI/WebAPI/DataTO/CreateItemDTO.cs
+++ WebAPI/WebAPI/DataTO/CreateItemDTO.cs
@@ -4,10 +4,13 @@ namespace WebAPI.DataTO
 {
     public record CreateItemDTO
     {
+
+        
         [Required]
         public string Name { get; init; }
         [Required]
         [Range(6,100)]
         public decimal Price { get; init; }
+        
     }
 }
